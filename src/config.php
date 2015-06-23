5a2b536ed5cc8f1f11ac1b81c8748d9249bdb970
Clinton Andrews <candrews800@gmail.com>
2015-06-23 13:33:42 -0400

    update config file

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_url' => 'http://zipcode.app',
-    'api_key' => '$2y$10$dWxan3fVpbW3zY2AZiTNzeY9279clPmA7397iKTaqdx19RrPQlaLq'
+    'api_url' => 'http://zzipcode.com',
+    'api_key' => 'your api key'
 ];
